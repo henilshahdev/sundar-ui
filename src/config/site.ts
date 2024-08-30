1bb30d6c580d1027da4c3e330033e3986197ba48
draw-fast <info.drawfast@gmail.com>
2024-08-29 22:27:10 -0700

    Added shadcn + SEO stuff

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "sundar-ui",
+	url: "https://sundar-ui.vercel.app",
+	ogImage: "https://sundar-ui.vercel.app/og.jpg",
+	description:
+		"Beautifully designed shadcn/ui templates that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+	links: {
+		twitter: "https://twitter.com/henil106",
+		github: "https://github.com/henilshahdev/sundar-ui",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
---
+++ src/lib/fonts.tsx
@@ -0,0 +1,7 @@
+// import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+import { Figtree as FontMono } from "next/font/google";
+// import { GeistMono } from "geist/font/mono"
+
+export const fontMono = FontMono({
+	subsets: ["latin"],
+});
