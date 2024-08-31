b8e0d4260b2c7401687230b432ad0cb823a83bfa
draw-fast <info.drawfast@gmail.com>
2024-08-30 21:56:55 -0700

    Hotfix: Trigger Deploy

--- src/lib/elements.ts
+++ src/lib/elements.ts
@@ -1,15 +1,15 @@
-import { elementSchema } from "./schema";
+// import { elementSchema } from "./schema";
 
-export async function getBlock(name: string) {
-	const entry = Index[name];
+// export async function getBlock(name: string) {
+// 	const entry = Index[name];
 
-	// const content = await _getBlockContent(name, style);
+// 	// const content = await _getBlockContent(name, style);
 
-	return elementSchema.parse({
-		code: content.code ? await highlightCode(content.code) : "",
-		...entry,
+// 	return elementSchema.parse({
+// 		code: content.code ? await highlightCode(content.code) : "",
+// 		...entry,
 
-		description: content.description || "",
-		type: "components:block",
-	});
-}
+// 		description: content.description || "",
+// 		type: "components:block",
+// 	});
+// }
